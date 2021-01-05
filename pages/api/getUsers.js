7229b93b662017ddf6dad19b3c07761a185fcef3
CrisRonda <cristian.j005@gmail.com>
2021-01-05 18:28:34 -0500

    update getUsers

--- pages/api/getUsers.js
+++ pages/api/getUsers.js
@@ -1,14 +1,12 @@
 import { getAuthUsers, verifyIdToken } from "../../utils/auth/firebaseAdmin";
-const favoriteFoods = ["pizza", "burger", "chips", "tortilla"];
 
-const getFood = async (req, res) => {
+const getUsers = async (req, res) => {
   const token = req.headers.token;
 
   try {
     await verifyIdToken(token);
     const users = await getAuthUsers();
     return res.status(200).json({
-      food: favoriteFoods[Math.floor(Math.random() * favoriteFoods.length)],
       users: users,
     });
   } catch (error) {
@@ -16,4 +14,4 @@ const getFood = async (req, res) => {
   }
 };
 
-export default getFood;
+export default getUsers;
