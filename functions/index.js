7d1c318b81f6d6e3cd06455472de33c146198206
CrisRonda <cristian.j005@gmail.com>
2021-01-05 08:23:29 -0500

    integrate algolia and firebase

---
+++ functions/index.js
@@ -0,0 +1,40 @@
+const functions = require("firebase-functions");
+const keyFilename = require("./service-account.json");
+const admin = require("firebase-admin");
+const { default: algoliasearch } = require("algoliasearch");
+const algoliaConfig = {
+  appid: "2J3H4NW9CQ",
+  apikey: "5bbc35f573e6b521ec9fd0c1b12cba7c",
+};
+admin.initializeApp();
+
+const clientAlgolia = algoliasearch(algoliaConfig.appid, algoliaConfig.apikey);
+const collectionIndex = clientAlgolia.initIndex("files_search");
+exports.algolia = functions.storage.object().onFinalize(async (object) => {
+  try {
+    const { size, metadata, name } = object;
+    const refFiles = admin.firestore().collection("files").doc();
+    const id = refFiles.id;
+    const file = { id, size, metadata, name };
+    await refFiles.set(file);
+  } catch (error) {
+    console.log("*************************");
+    console.log("error", error);
+    console.log("*************************");
+  }
+});
+async function saveDocumentInAlgolia(snapshot) {
+  if (snapshot.exists) {
+    const record = snapshot.data();
+    if (record) {
+      record.objectID = snapshot.id;
+      const { objectID } = await collectionIndex.saveObject(record);
+      console.log(objectID);
+    }
+  }
+}
+exports.collectionOnCreate = functions.firestore
+  .document("files/{uid}")
+  .onCreate(async (snapshot) => {
+    await saveDocumentInAlgolia(snapshot);
+  });
